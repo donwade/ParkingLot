1e5d5d881bdfd582216082f05a0a64c90e24e462
Don Wade <don_wade@rogers.com>
2025-08-04 12:24:48 -0400

    battery

---
+++ StationBattery/StationBattery.ino
@@ -0,0 +1,33 @@
+//#https://docs.m5stack.com/en/arduino/m5station/battery
+
+#include "M5Unified.h"
+
+void setup()
+{
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    M5.Display.setTextDatum(middle_center);
+    M5.Display.setTextColor(TFT_BLACK);
+    M5.Display.setTextFont(&fonts::FreeSansOblique12pt7b);
+    M5.Display.setTextSize(2);
+}
+
+void loop()
+{
+    M5.Display.clear(TFT_WHITE);
+    
+    bool isCharging = M5.Power.isCharging();
+    int vol_per = M5.Power.getBatteryLevel();
+    int vol = M5.Power.getBatteryVoltage();
+    int cur = M5.Power.getBatteryCurrent();
+
+    M5.Display.setCursor(0, 30);
+    M5.Display.printf("Charging: %s \n\n", isCharging ? "Yes" : "No");
+    M5.Display.setCursor(0, 60);
+    M5.Display.printf("Bat_level: %d%%", vol_per);
+    M5.Display.setCursor(0, 90);
+    M5.Display.printf("Bat_voltage: %d%mV", vol);
+    M5.Display.setCursor(0, 120);
+    M5.Display.printf("Bat_current: %d%mA", cur);
+    delay(2000);
+}                                                
